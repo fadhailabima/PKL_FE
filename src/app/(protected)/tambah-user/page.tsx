d719dbe6fe27e26a8aa11d7dd2a645eee1a995d8
Bima <fadhailabima@students.undip.ac.id>
2024-03-07 13:49:07 +0700

    revisi lagi

--- src/app/(protected)/tambah-user/page.tsx
+++ src/app/(protected)/tambah-user/page.tsx
@@ -105,7 +105,7 @@ export default function Profile() {
                   Admin
                 </DropdownMenuRadioItem>
                 <DropdownMenuRadioItem value="karyawan">
-                  Petugas
+                  Karyawan
                 </DropdownMenuRadioItem>
               </DropdownMenuRadioGroup>
             </DropdownMenuContent>
